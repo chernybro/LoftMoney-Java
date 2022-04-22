8822e1e9c5dec9d4f374cb50df5d15a505444cbc
chernybro <anvar-1234567@yandex.ru>
2022-04-22 20:37:21 +0300

    Добавил слушателя на одно из текстовых полей

--- app/src/main/java/com/chernybro/loftmoneyjava/AddItemActivity.java
+++ app/src/main/java/com/chernybro/loftmoneyjava/AddItemActivity.java
@@ -1,6 +1,10 @@
 package com.chernybro.loftmoneyjava;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +15,28 @@ public class AddItemActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        Button button = findViewById(R.id.add_button);
+        EditText textIncomes = findViewById(R.id.incomes_tv);
+        textIncomes.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editable.toString().equals("")) {
+                    button.setEnabled(true);
+                } else {
+                    button.setEnabled(false);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/chernybro/loftmoneyjava/MainActivity.java
+++ app/src/main/java/com/chernybro/loftmoneyjava/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+// Hotkeys
+// ctrl a - выделить всё
+// ctrl c ctrl v ctrl x
+// ctrl z
+
+// ctrl d дублировать строку
+// shift shift поиск по проекту
+// ctrl f поиск по файлу
+// ctrl shift f поиск по файлам в проекте
+// tab - автозаполнение
+// alt enter - помощь от студии
+//
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, AddItemActivity.class);
-        startActivity(intent);
+        Intent activity = new Intent(this, AddItemActivity.class);
+
+        startActivity(activity);
+
+
     }
 }
